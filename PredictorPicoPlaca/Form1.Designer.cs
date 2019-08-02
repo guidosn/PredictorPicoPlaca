bac5a253e43eb008553eb93097087aaa2b468cc5
guidosn <guidosn@hotmail.com>
2019-08-02 10:13:02 -0500

    Add files via upload

---
+++ PredictorPicoPlaca/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace PredictorPicoPlaca
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtPlaca = new System.Windows.Forms.TextBox();
+            this.TxtFecha = new System.Windows.Forms.TextBox();
+            this.TxtHora = new System.Windows.Forms.TextBox();
+            this.BtnNuevo = new System.Windows.Forms.Button();
+            this.BtnDetectar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Lblresultado = new System.Windows.Forms.Label();
+            this.Lblmensajedia = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TxtPlaca
+            // 
+            this.TxtPlaca.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtPlaca.Location = new System.Drawing.Point(97, 39);
+            this.TxtPlaca.Name = "TxtPlaca";
+            this.TxtPlaca.Size = new System.Drawing.Size(214, 40);
+            this.TxtPlaca.TabIndex = 0;
+            // 
+            // TxtFecha
+            // 
+            this.TxtFecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtFecha.Location = new System.Drawing.Point(97, 85);
+            this.TxtFecha.Name = "TxtFecha";
+            this.TxtFecha.Size = new System.Drawing.Size(214, 40);
+            this.TxtFecha.TabIndex = 1;
+            // 
+            // TxtHora
+            // 
+            this.TxtHora.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtHora.Location = new System.Drawing.Point(97, 131);
+            this.TxtHora.Name = "TxtHora";
+            this.TxtHora.Size = new System.Drawing.Size(214, 40);
+            this.TxtHora.TabIndex = 2;
+            // 
+            // BtnNuevo
+            // 
+            this.BtnNuevo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnNuevo.Location = new System.Drawing.Point(350, 85);
+            this.BtnNuevo.Name = "BtnNuevo";
+            this.BtnNuevo.Size = new System.Drawing.Size(135, 40);
+            this.BtnNuevo.TabIndex = 4;
+            this.BtnNuevo.Text = "Nuevo";
+            this.BtnNuevo.UseVisualStyleBackColor = true;
+            this.BtnNuevo.Click += new System.EventHandler(this.BtnNuevo_Click);
+            // 
+            // BtnDetectar
+            // 
+            this.BtnDetectar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnDetectar.Location = new System.Drawing.Point(350, 131);
+            this.BtnDetectar.Name = "BtnDetectar";
+            this.BtnDetectar.Size = new System.Drawing.Size(135, 40);
+            this.BtnDetectar.TabIndex = 3;
+            this.BtnDetectar.Text = "Detectar";
+            this.BtnDetectar.UseVisualStyleBackColor = true;
+            this.BtnDetectar.Click += new System.EventHandler(this.BtnDetectar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Placa:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Fecha:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(21, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 24);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Hora:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Lblresultado
+            // 
+            this.Lblresultado.AutoSize = true;
+            this.Lblresultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lblresultado.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.Lblresultado.Location = new System.Drawing.Point(12, 261);
+            this.Lblresultado.Name = "Lblresultado";
+            this.Lblresultado.Size = new System.Drawing.Size(183, 39);
+            this.Lblresultado.TabIndex = 3;
+            this.Lblresultado.Text = "Resultado:";
+            // 
+            // Lblmensajedia
+            // 
+            this.Lblmensajedia.AutoSize = true;
+            this.Lblmensajedia.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.Lblmensajedia.Location = new System.Drawing.Point(22, 213);
+            this.Lblmensajedia.Name = "Lblmensajedia";
+            this.Lblmensajedia.Size = new System.Drawing.Size(37, 24);
+            this.Lblmensajedia.TabIndex = 5;
+            this.Lblmensajedia.Text = "Es:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 381);
+            this.Controls.Add(this.Lblmensajedia);
+            this.Controls.Add(this.Lblresultado);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnDetectar);
+            this.Controls.Add(this.BtnNuevo);
+            this.Controls.Add(this.TxtHora);
+            this.Controls.Add(this.TxtFecha);
+            this.Controls.Add(this.TxtPlaca);
+            this.Name = "Form1";
+            this.Text = "Predictor de Pico y Placa";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtPlaca;
+        private System.Windows.Forms.TextBox TxtFecha;
+        private System.Windows.Forms.TextBox TxtHora;
+        private System.Windows.Forms.Button BtnNuevo;
+        private System.Windows.Forms.Button BtnDetectar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label Lblresultado;
+        private System.Windows.Forms.Label Lblmensajedia;
+    }
+}
+
---
+++ PredictorPicoPlaca/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace PredictorPicoPlaca
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnNuevo_Click(object sender, EventArgs e)
+        {
+            DateTime fecha = DateTime.Now;
+            TxtPlaca.Text = Lblresultado.Text = Lblmensajedia.Text= "";
+            TxtFecha.Text = fecha.ToString("dd/MM/yyyy");
+            TxtHora.Text = fecha.ToString("HH:mm:ss");
+            TxtPlaca.Focus();
+        }
+
+        private void BtnDetectar_Click(object sender, EventArgs e)
+        {
+            if (TxtPlaca.Text != string.Empty)
+                Lblresultado.Text = Predictor_pico_y_placa(TxtPlaca.Text,TxtFecha.Text,TxtHora.Text);
+        }
+
+        private string Predictor_pico_y_placa(string _placa, string _fecha, string _hora)
+        {
+            string mensaje="Vehículo, SI puede circular";
+            string mensaje1 = "";
+
+            DateTime fecha= Convert.ToDateTime(_fecha, new CultureInfo("es-ES"));
+
+            int dia = (int)fecha.DayOfWeek;
+
+            char digito_placa=Convert.ToChar(_placa.Substring(_placa.Length-1,1));
+
+            int hora = Convert.ToInt32(_hora.Substring(0,2));
+            int minuto= Convert.ToInt32(_hora.Substring(3, 2));
+
+            char auto1_nocircula;
+            char auto2_nocircula;
+
+            switch (dia)
+            {
+                case 1: //LUNES
+                    mensaje1 = "Es, LUNES:";
+                    auto1_nocircula = '1';
+                    auto2_nocircula = '2';
+                    break;
+                case 2: //MARTES
+                    mensaje1 = "Es, MARTES:";
+                    auto1_nocircula = '3';
+                    auto2_nocircula = '4';
+                    break;
+                case 3: //MIERCOLES
+                    mensaje1 = "Es, MIERCOLES:";
+                    auto1_nocircula = '5';
+                    auto2_nocircula = '6';
+                    break;
+                case 4: //JUEVES
+                    mensaje1 = "Es, JUEVES:";
+                    auto1_nocircula = '7';
+                    auto2_nocircula = '8';
+                    break;
+                case 5: //VIERNES
+                    mensaje1 = "Es, VIERNES:";
+                    auto1_nocircula = '9';
+                    auto2_nocircula = '0';
+                    break;
+
+                default:
+                    mensaje1 = "Es, SABADO o DOMINGO";
+                    auto1_nocircula = 'x';
+                    auto2_nocircula = 'x';
+                    mensaje = "El vehículo puede circular";
+                    break;
+
+            }
+
+            if (auto1_nocircula==digito_placa || auto2_nocircula==digito_placa)
+            {
+
+                switch(hora)
+                {
+                    case 7:
+                    case 8:
+                        mensaje = "El vehículo NO puede circular";
+                        break;
+                    case 9:
+                        if (minuto <= 30)
+                            mensaje = "El vehículo NO puede circular";
+                        break;
+                    case 16:
+                    case 17:
+                    case 18:
+                        mensaje = "El vehículo NO puede circular";
+                        break;
+                    case 19:
+                        if (minuto <= 30)
+                            mensaje = "El vehículo NO puede circular";
+                        break;
+
+                }
+
+            }
+
+
+            Lblmensajedia.Text = mensaje1;
+
+            return mensaje;
+        }
+     
+    }
+}
